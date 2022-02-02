759d01ef12dc2bbdfffa95e658a7590fbe174d71
Vsevolod Yakunin <v.yakunin@it.ecommpay.com>
2022-02-02 18:40:18 +0300

    add some mocks for tasks

--- application/service/service.go
+++ application/service/service.go
@@ -1,7 +1,21 @@
 package service
 
+import (
+	"github.com/vsyakunin/sg-task/domain/models"
+)
+
 type Service struct{}
 
 func NewService() *Service {
 	return &Service{}
+}
+
+func (svc *Service) GetAllTasks() ([]models.Task, error) {
+	const funcName = "service.GetAllTasks"
+
+	var tasks []models.Task
+
+	tasks = models.AllTasks
+
+	return tasks, nil
 }
--- domain/models/task.go
+++ domain/models/task.go
@@ -11,3 +11,36 @@ type Task struct {
 	StartedAt time.Time `json:"started_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+var AllTasks = []Task{TaskOne, TaskTwo, TaskThree}
+
+var TaskOne = Task{
+	ID:        1,
+	UserID:    1,
+	Name:      "task one",
+	Category:  "find a doctor",
+	Status:    "processing",
+	StartedAt: time.Now(),
+	UpdatedAt: time.Now(),
+}
+
+var TaskTwo = Task{
+	ID:        2,
+	UserID:    1,
+	Name:      "task two",
+	Category:  "buy plane tickets",
+	Status:    "processing",
+	StartedAt: time.Now(),
+	UpdatedAt: time.Now(),
+}
+
+var TaskThree = Task{
+	ID:        3,
+	UserID:    2,
+	Name:      "task three",
+	Category:  "find a dog sitter",
+	Status:    "processing",
+	StartedAt: time.Now(),
+	UpdatedAt: time.Now(),
+}
+
